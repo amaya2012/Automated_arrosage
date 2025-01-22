9522da12d429f5fd7473fbc6cd33d6212f2213ea
Amaya <amaya.weyer@gmail.com>
2025-01-22 10:22:37 +0100

    Ajout wifi Modification activation relais

--- src/main.c
+++ src/main.c
@@ -6,31 +6,55 @@
 #define DUREE 10
 
 #define PIN_SECURE 23  // Pin pour la détection d'eau
-#define PIN_POMPE 19   // Pin pour l'activation de la pompe
+#define PIN_RELAIS 19   // Pin pour l'activation de la pompe
 #define LED_BUILTIN 2  // LED intégrée
 
 int flag = 1; // Variable pour éviter des activations multiples
+const char* ssid = "fbx_buceo";
+const char* password = "1970C1998A";
 
 // Fonction pour vérifier le capteur de sécurité d'eau
 bool isWaterAvailable() {
   int buttonState = digitalRead(PIN_SECURE); // Lecture de l'état du capteur
-  return buttonState == HIGH; // Si HIGH, l'eau est disponible
+  return buttonState == LOW; // Si LOW, l'eau est disponible
 }
 
 // Fonction pour activer la pompe
 void activatePump(int t) {
   digitalWrite(LED_BUILTIN, HIGH); // Active la pompe
+  //digitalWrite(PIN_RELAIS, HIGH); // Active la pompe
   delay(1000 * t);              // Attente de t secondes
   digitalWrite(LED_BUILTIN, LOW); // Arrête la pompe
+  //digitalWrite(PIN_RELAIS, LOW); // Arrête la pompe
+}
+
+void connectWifi(){
+  WiFi.mode(WIFI_STA); 
+  WiFi.begin(ssid, password);
+  Serial.println("\nConnecting");
+
+  while(WiFi.status() != WL_CONNECTED){
+      Serial.print(".");
+      delay(100);
+  }
+
+  Serial.println("\nConnected to the WiFi network");
+  Serial.print("Local ESP32 IP: ");
+  Serial.println(WiFi.localIP());
 }
 
 void setup() {
   Serial.begin(115200);              // Initialisation de la communication série
-  pinMode(PIN_SECURE, INPUT_PULLUP); // Configure GPIO23 en entrée avec résistance pull-up interne
-  pinMode(PIN_POMPE, OUTPUT);        // Configure GPIO19 en sortie
-  pinMode(LED_BUILTIN, OUTPUT);      // Configure la LED intégrée en sortie
+  delay(1000);
 
+  // Initilaisation des Pins
+  pinMode(PIN_SECURE, INPUT_PULLUP); // Configure GPIO23 en entrée avec résistance pull-up interne
+  pinMode(PIN_RELAIS, OUTPUT);        // Configure GPIO19 en sortie
+  pinMode(LED_BUILTIN, OUTPUT);      // Configure la LED intégrée en sortie
   Serial.println("Initialisation du système...");
+
+  // Initialisation du WIFI
+  connectWifi();
 }
 
 void loop() {
